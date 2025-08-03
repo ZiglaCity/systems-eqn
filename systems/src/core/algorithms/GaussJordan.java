304ef608189c7944cafccc8089616a7829565def
ZiglaCity <ziglacity@gmail.com>
2025-08-03 00:19:39 +0000

    setup

---
+++ systems/src/core/algorithms/GaussJordan.java
@@ -0,0 +1,7 @@
+package core.algorithms;
+
+public class GaussJordan {
+    public static void GaussElimination(){
+        System.out.println("Starting the Gauss Elimination...");
+    }
+}
---
+++ systems/src/core/utils/Utils.java
@@ -0,0 +1,8 @@
+package core.utils;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Utils {
+
+}
